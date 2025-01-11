380f05ea6b26f0ce77c7e1d9b662e93e922d3442
SuperSandyman <66544945+SuperSandyman@users.noreply.github.com>
2025-01-11 23:48:35 +0900

    rustlings quiz2

--- rustlings/exercises/quizzes/quiz2.rs
+++ rustlings/exercises/quizzes/quiz2.rs
@@ -27,7 +27,28 @@ mod my_module {
     use super::Command;
 
     // TODO: Complete the function as described above.
-    // pub fn transformer(input: ???) -> ??? { ??? }
+    pub fn transformer(input: Vec<(String, Command)>) -> Vec<String> {
+        let mut output = Vec::new();
+
+        for (text, command) in input {
+            let result = match command {
+                Command::Uppercase => text.to_uppercase(),
+                Command::Trim => text.trim().to_string(),
+                Command::Append(times) => {
+                    // `text`に"bar"を`times`回追加
+                    let mut appended_string = text.clone();
+                    for _ in 0..times {
+                        appended_string.push_str("bar");
+                    }
+                    appended_string
+                },
+            };
+
+            output.push(result);
+        }
+
+        output
+    }
 }
 
 fn main() {
@@ -37,8 +58,8 @@ fn main() {
 #[cfg(test)]
 mod tests {
     // TODO: What do we need to import to have `transformer` in scope?
-    // use ???;
-    use super::Command;
+    use super::*;
+    use my_module::transformer;
 
     #[test]
     fn it_works() {
